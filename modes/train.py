5decc5135e3a0bc6ad512b273ba1a92c90bc11ff
Stefan Pantic <stefanpantic13@gmail.com>
2020-05-13 21:40:35 +0200

    Started implementing train function (with placeholder dataset)

--- modes/train.py
+++ modes/train.py
@@ -1,7 +1,12 @@
 import click
 import tensorflow as tf
+import numpy as np
 
 from models.jasper import Jasper
+from models.quartznet import QuartzNet
+from utilities.losses import ctc_loss
+from utilities.optimizers import NovoGrad
+from utilities.wrappers import AutomaticLossScaler, MixedPrecisionOptimizerWrapper
 
 
 @click.command(name='train', help='Train ASR model.')
@@ -11,12 +16,49 @@ from models.jasper import Jasper
 @click.option('--r', default=5, help='Jasper "R" parameter.')
 @click.option('--lr', default=1e-3, help='Model learning rate.')
 @click.option('--batch_size', default=16, help='Model batch size.')
+@click.option('--epochs', default=400, help='Model batch size.')
 def train(**options):
-    with tf.Session() as sess:
-        # TODO: Test model implementation.
-        input_ph = tf.placeholder(dtype=tf.float32, shape=[None, None, 64], name='input')
-        jasper = Jasper(b=1, r=3)
-        logits = jasper(input_ph)
-        writer = tf.summary.FileWriter('./logs', sess.graph)
-        print(jasper.get_number_of_trainable_variables())
+    # TODO: Load dataset
+    mfcc = np.ones((1000, 3000, 64), dtype=np.float32)
+    seq_lens = np.ones((1000,), dtype=np.int32)
+    labels = np.ones((1000, 300), dtype=np.int32)
 
+    # Create dataset from input tensors
+    dataset = tf.data.Dataset.from_tensor_slices((mfcc, seq_lens, labels))
+
+    # Prepare datasets
+    dataset = dataset.repeat(options['epochs']).batch(options['batch_size'])
+    train_it = dataset.make_one_shot_iterator()
+    mfcc_inputs, seq_lens_inputs, labels_inputs = train_it.get_next()
+    labels_inputs = tf.contrib.layers.dense_to_sparse(labels_inputs)
+
+    quartz = QuartzNet(b=1, r=3)
+    logits = quartz(mfcc_inputs)
+
+    # Calculate CTC loss
+    ctc = ctc_loss(logits, labels_inputs, seq_lens_inputs)
+
+    # Setup optimizer
+    with tf.name_scope('optimizer'):
+        g_step = tf.train.get_or_create_global_step()
+        lr = tf.train.polynomial_decay(learning_rate=options['lr'],
+                                       global_step=g_step,
+                                       decay_steps=int(1e7),
+                                       end_learning_rate=1e-7,
+                                       power=2.0)
+        novo_grad = NovoGrad(learning_rate=lr)
+        scaler = AutomaticLossScaler(algorithm='backoff')
+        optimizer = MixedPrecisionOptimizerWrapper(novo_grad, scaler)
+        train_op = optimizer.minimize(ctc)
+
+    # Create session config
+    gpu_options = tf.GPUOptions(allow_growth=True)
+    tf_config = tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options)
+
+    # Create training hooks
+    with tf.train.MonitoredTrainingSession(checkpoint_dir=options['log_dir'],
+                                           config=tf_config,
+                                           save_summaries_steps=5) as sess:
+        # Run training loop
+        while not sess.should_stop():
+            sess.run([train_op])
