a90f300eabaabfb3058adaf684daa0dcb1b02ba7
Stefan Pantic <stefanpantic13@gmail.com>
2020-05-17 21:10:28 +0200

    Implemented basic training loop

--- modes/train.py
+++ modes/train.py
@@ -1,9 +1,12 @@
+import os
+
 import click
 import tensorflow as tf
 import numpy as np
 
 from models.jasper import Jasper
 from models.quartznet import QuartzNet
+from utilities.datasets.read import create_train_inputs, create_val_inputs
 from utilities.losses import ctc_loss
 from utilities.optimizers import NovoGrad
 from utilities.wrappers import AutomaticLossScaler, MixedPrecisionOptimizerWrapper
@@ -11,32 +14,31 @@ from utilities.wrappers import AutomaticLossScaler, MixedPrecisionOptimizerWrapp
 
 @click.command(name='train', help='Train ASR model.')
 @click.option('--dataset', required=True, help='Path to training dataset.')
-@click.option('--log_dir', default='./logs/jasper', help='Where to log weights and graphs.')
-@click.option('--b', default=1, help='Jasper "B" parameter.')
-@click.option('--r', default=5, help='Jasper "R" parameter.')
+@click.option('--log_dir', default='./logs/train', help='Where to log weights and graphs.')
+@click.option('--b', default=1, help='Model "B" parameter.')
+@click.option('--r', default=5, help='Model "R" parameter.')
 @click.option('--lr', default=1e-3, help='Model learning rate.')
 @click.option('--batch_size', default=16, help='Model batch size.')
 @click.option('--epochs', default=400, help='Model batch size.')
 def train(**options):
-    # TODO: Load dataset
-    mfcc = np.ones((1000, 3000, 64), dtype=np.float32)
-    seq_lens = np.ones((1000,), dtype=np.int32)
-    labels = np.ones((1000, 300), dtype=np.int32)
+    if not os.path.exists(options['dataset']):
+        raise ValueError(f'Invalid option for --dataset, directory: {options["dataset"]} doesn\'t exist.')
 
-    # Create dataset from input tensors
-    dataset = tf.data.Dataset.from_tensor_slices((mfcc, seq_lens, labels))
-
-    # Prepare datasets
-    dataset = dataset.repeat(options['epochs']).batch(options['batch_size'])
-    train_it = dataset.make_one_shot_iterator()
-    mfcc_inputs, seq_lens_inputs, labels_inputs = train_it.get_next()
-    labels_inputs = tf.contrib.layers.dense_to_sparse(labels_inputs)
+    # Prepare train dataset
+    mfcc_train_ins, labels_train_ins, seq_lens_train_ins = create_train_inputs(data_dir=options['dataset'],
+                                                                               batch_size=options['batch_size'],
+                                                                               epochs=options['epochs'], shuffle=True)
+    # Prepare validation dataset
+    mfcc_val_ins, labels_val_ins, seq_lens_val_ins = create_val_inputs(data_dir=options['dataset'])
 
     quartz = QuartzNet(b=1, r=3)
-    logits = quartz(mfcc_inputs)
+    train_logits = quartz(mfcc_train_ins)
+
+    # TODO: Calculate and log validation metrics
+    val_logits = quartz(mfcc_val_ins)
 
     # Calculate CTC loss
-    ctc = ctc_loss(logits, labels_inputs, seq_lens_inputs)
+    ctc = ctc_loss(train_logits, labels_train_ins, seq_lens_train_ins)
 
     # Setup optimizer
     with tf.name_scope('optimizer'):
@@ -58,7 +60,13 @@ def train(**options):
     # Create training hooks
     with tf.train.MonitoredTrainingSession(checkpoint_dir=options['log_dir'],
                                            config=tf_config,
-                                           save_summaries_steps=5) as sess:
+                                           summary_dir=None) as sess:
+        count = 0
         # Run training loop
         while not sess.should_stop():
+            # TODO: Calculate and log train metrics
             sess.run([train_op])
+            count += 1
+            if count % 100 == 0:
+                print(f'Executed {count} batches...')
+
