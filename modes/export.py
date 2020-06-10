f27264170b8ea1b8063ef51ce64a9ca8a2d6f42c
Stefan Pantic <stefanpantic13@gmail.com>
2020-06-10 22:05:20 +0200

    Fixed export mode

--- modes/export.py
+++ modes/export.py
@@ -2,27 +2,46 @@ import os
 
 import click
 import tensorflow as tf
-from tensorflow.python.tools import freeze_graph, optimize_for_inference_lib
+from tensorflow.python.tools import optimize_for_inference_lib
+from tensorflow.keras import backend as K
 
-from utilities.graph import load_graph
+from models.jasper import Jasper
+from models.quartznet import QuartzNet
+from utilities.decoders import beam_search_decoder
 
 
 @click.command(name='export', help='Export Tensorflow model to a format used for inference.')
 @click.option('--model_dir', required=True, help='Path to serialized model.')
 @click.option('--output_dir', default=None, help='Where to output converted model.')
 def export(**options):
-    # Freeze graph
-    latest_checkpoint = tf.train.latest_checkpoint(options['model_dir'])
-    freeze_graph.freeze_graph(os.path.join(options['model_dir'], 'graph.pbtxt'), "", False, latest_checkpoint,
-                              "output", 'save/restore_all', 'save/Const:0',
-                              os.path.join(options['model_dir'], 'frozen_graph.pb'), True, '')
-    # Optimize graph
-    frozen_graph = load_graph(os.path.join(options['model_dir'], 'frozen_graph.pb'), prefix='export')
-    optimized_graph = optimize_for_inference_lib.optimize_for_inference(frozen_graph.as_graph_def(),
-                                                                        ["export/input"],  # an array of input node(s)
-                                                                        ["export/output"],  # an array of output node(s)
-                                                                        tf.int32.as_datatype_enum)
+    # Model class catalogue
+    models = {
+        'quartznet': QuartzNet,
+        'jasper': Jasper,
+    }
+
+    # Construct model
+    K.set_learning_phase(0)
+    input_ph = tf.placeholder(dtype=tf.float32, shape=[None, 64], name='input')
+    expanded_ph = tf.expand_dims(input_ph, axis=0)
+    model = QuartzNet(b=1, r=5)
+    logits = model(expanded_ph, training=False)
+    prep_conv_size = model.get_layer_configuration()['prep_config']['kernel_size']
+    outputs = beam_search_decoder(logits, tf.expand_dims(tf.shape(expanded_ph)[1], axis=0),
+                                  prep_conv_kernel_size=prep_conv_size, to_dense=True)
+    _ = tf.identity(outputs, name='output')
+
+    # Restore model weights
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess, tf.train.latest_checkpoint(options['model_dir']))
+
+    # Convert variables to constants
+    constant_graph = tf.graph_util.convert_variables_to_constants(sess,
+                                                                  sess.graph_def,
+                                                                  ['output'],
+                                                                  ['input', 'output'])
     # Serialize optimized graph
     output_dir = options['output_dir'] or options['model_dir']
-    with tf.gfile.FastGFile(os.path.join(output_dir, 'optimized_graph.pb'), 'w') as f:
-        f.write(optimized_graph.SerializeToString())
+    with tf.gfile.FastGFile(os.path.join(output_dir, 'frozen_graph.pb'), 'w') as f:
+        f.write(constant_graph.SerializeToString())
